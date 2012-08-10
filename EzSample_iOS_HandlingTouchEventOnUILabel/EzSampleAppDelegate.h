69a6f1dc97a1206e6f08789b5f542766e269cdf4
Tomohiro Kumagai <tomohiro@ez-style.jp>
2012-08-10 11:10:48 +0900

    Initial Commit

---
+++ EzSample_iOS_HandlingTouchEventOnUILabel/EzSampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EzSampleAppDelegate.h
+//  EzSample_iOS_HandlingTouchEventOnUILabel
+//
+//  Created by 熊谷 友宏 on H.24/08/10.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EzSampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EzSample_iOS_HandlingTouchEventOnUILabel/EzSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  EzSampleViewController.h
+//  EzSample_iOS_HandlingTouchEventOnUILabel
+//
+//  Created by 熊谷 友宏 on H.24/08/10.
+//  Copyright (c) 平成24年 Tomohiro Kumagai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EzSampleViewController : UIViewController
+
+@end
